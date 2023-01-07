e4679a6694cf8dc09a7125aa07114662710da93b
irfanakbari <irfanakbarihabibi18@gmail.com>
2023-01-07 22:23:39 +0700

    Add icon launcher and leaderboard logic

--- app/src/main/java/com/example/utsmobileprogramming/utility/FirebaseService.kt
+++ app/src/main/java/com/example/utsmobileprogramming/utility/FirebaseService.kt
@@ -1,22 +1,46 @@
 package com.example.utsmobileprogramming.utility
 
-import android.content.ContentValues.TAG
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
 class FirebaseService {
     private val db = Firebase.firestore
+    private val currentUser = FirebaseAuth.getInstance().currentUser
 
-    fun saveSkor(skor: HashMap<String, Int>, username:String, type: String){
-        db.collection(username).document(type)
-            .set(skor)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: $documentReference")
+
+    fun saveSkor(skor: Int?, type: String?) {
+        if (currentUser != null) {
+            val uid = currentUser.uid
+            val docRef = db.collection(type.toString()).document("skor")
+            docRef.get().addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.get(uid) != null) {
+                    Log.d("Firestore","uda Ada nih gan  $skor")
+                    val oldValue = documentSnapshot.get(uid)
+                    if (skor != null) {
+                        if (oldValue.toString().toInt() < skor) {
+                            Log.d("Firestore"," Lebih kecil gan $oldValue <  $skor $uid")
+                            docRef.update(uid,skor)
+                        }
+                    }
+                } else {
+                    Log.d("Firestore","baru Ada nih gan")
+                    docRef.set(hashMapOf(uid to skor), SetOptions.merge())
+                }
             }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
             }
+        getAllData()
+        }
+
+    fun getAllData(): List<Map.Entry<String, Any>> {
+        val docRef = db.collection("Divisor").document("skor")
+        var sortedByValue: List<Map.Entry<String, Any>> = emptyList()
+        docRef.get().addOnSuccessListener{ datas ->
+            sortedByValue = datas.data!!.entries.sortedBy { (_, value) -> value.toString().toInt() }
+        }
+        return sortedByValue
     }
 }
